359d48136e898cdbb318d902ee0e18fd73bab6b0
JavierManjónMartos <jmanjonmartos@gmail.com>
2025-07-07 18:52:29 +0200

    Mogut ReunioController a carpeta Api

--- app/Http/Controllers/Api/ReunioController.php
+++ app/Http/Controllers/Api/ReunioController.php
@@ -1,10 +1,12 @@
 <?php
 
-namespace App\Http\Controllers;
+namespace App\Http\Controllers\Api;
 
 use App\Models\Reunio;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use App\Http\Controllers\Controller;
+
 
 class ReunioController extends Controller
 {
