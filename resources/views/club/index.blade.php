07804ba585cce6f76f121f7d796e7ea0761669e4
JavierManjónMartos <jmanjonmartos@gmail.com>
2025-07-23 18:58:12 +0200

    Implementada nuevos colores club - primera pagina

--- resources/views/club/index.blade.php
+++ resources/views/club/index.blade.php
@@ -7,12 +7,20 @@
 @section('content')
 
 <header class="text-center py-4">
-    <h1>CLUB DE FUTBOL - ESCOLA </h1>
+    <h1>CLUB DE FUTBOL - ESCOLA DE FUTBOL BASE <h1>
 </header>
-
 <div class="container mt-4">
-    <nav class="menu-public">
-        <img src="{{ asset('imagesGeneral/LOGO-NEW01.png') }}" alt="Logo" width="80px">
+    {{--LOGO I ACCES  --}}
+    <div class="logo-acces">
+        <div class="logo-container">
+            <img src="{{ asset('imagesGeneral/LOGO-NEW01.png') }}" alt="Logo" class="logo-escola">
+        </div>
+        <div class="acces-usuaris">
+            <a href="{{ route('login') }}" class="btn-acces">Accés Usuaris</a>
+        </div>
+    </div>
+
+    <nav class="nav-publica">
         <hr class="linea">
         <ul>
             <li class="desplegable"><a href="{{ route('club.index') }}">CLUB</a>
@@ -25,7 +33,6 @@
                     <a href="{{ route('club.accesSoci') }}">Notificacions per a socis</a>
                 </div>
             </li>
-            <!-- Aquí continues amb ESCOLA, 1ER EQUIP, 2ON EQUIP, etc. -->
             <li class="desplegable"><a href="{{ route('escola.index') }}">ESCOLA</a>
                 <div class="desplegableContingut">
                     <a href="{{ route('escola.formacio') }}">Formació</a>
@@ -36,58 +43,57 @@
                     <a href="{{ route('escola.accesCoordinador') }}">Accés Coordinador</a>
                 </div>
             </li>
-<li class="desplegable"><a href="{{ route('primer.index') }}">PRIMER EQUIP</a>
-    <div class="desplegableContingut">
-        <a href="{{ route('primer.plantilla') }}">Plantilla</a>
-        <a href="{{ route('primer.calendari') }}">Calendari</a>
-        <a href="{{ route('primer.jornada') }}">Jornada</a>
-        <a href="{{ route('primer.resultats') }}">Resultats</a>
-        <a href="{{ route('primer.classificacio') }}">Classificació</a>
-    </div>
-</li>
-<li class="desplegable"><a href="{{ route('segon.index') }}">SEGON EQUIP</a>
-    <div class="desplegableContingut">
-        <a href="{{ route('segon.plantilla') }}">Plantilla</a>
-        <a href="{{ route('segon.calendari') }}">Calendari</a>
-        <a href="{{ route('segon.jornada') }}">Jornada</a>
-        <a href="{{ route('segon.resultat') }}">Resultats</a>
-        <a href="{{ route('segon.classificacio') }}">Classificació</a>
-    </div>
-</li>
-<li class="desplegable">
-    <a href="{{ route('porters.index') }}">ESCOLA DE PORTERS</a>
-    <div class="desplegableContingut">
-        <a href="{{ route('porters.formacio') }}">Formació i metodologia</a>
-        <a href="{{ route('porters.horari') }}">Horari, calendari i esdeveniments</a>
-        <a href="{{ route('porters.entrenadors') }}">Entrenadors</a>
-        <a href="{{ route('porters.plans') }}">Plans i tarifes</a>
-        <a href="{{ route('porters.contacte') }}">Contacte</a>
-    </div>
-</li>
-<li class="desplegable"><a href="{{ route('secretaria.index') }}">SECRETARIA</a>
-    <div class="desplegableContingut">
-        <a href="{{ route('secretaria.oficina') }}">Oficina</a>
-        <a href="{{ route('secretaria.inscripcions') }}">Inscripcions</a>
-        <a href="{{ route('secretaria.merchandasing') }}">Merchandasing</a>
-        <a href="{{ route('secretaria.normativa') }}">Normativa</a>
-        <a href="{{ route('secretaria.contacte') }}">Contacte</a>
-        <a href="{{ route('secretaria.acces') }}">Accés Secretaria</a>
-    </div>
-</li>
-<li class="desplegable"><a href="{{ route('historia.index') }}">HISTÒRIA</a>
-    <div class="desplegableContingut">
-        <a href="{{ route('historia.ressenya') }}">Ressenya històrica</a>
-        <a href="{{ route('historia.cronologia') }}">Cronologia d'èxits</a>
-        <a href="{{ route('historia.fotografies') }}">Fotografies històriques</a>
-        <a href="{{ route('historia.envians') }}">Envia'ns la teva foto</a>
-    </div>
-</li>
-<li><a href="{{ route('patrocinadors.index') }}">PATROCINADORS</a></li>
-
-
+            <li class="desplegable"><a href="{{ route('primer.index') }}">PRIMER EQUIP</a>
+                <div class="desplegableContingut">
+                    <a href="{{ route('primer.plantilla') }}">Plantilla</a>
+                    <a href="{{ route('primer.calendari') }}">Calendari</a>
+                    <a href="{{ route('primer.jornada') }}">Jornada</a>
+                    <a href="{{ route('primer.resultats') }}">Resultats</a>
+                    <a href="{{ route('primer.classificacio') }}">Classificació</a>
+                </div>
+            </li>
+            <li class="desplegable"><a href="{{ route('segon.index') }}">SEGON EQUIP</a>
+                <div class="desplegableContingut">
+                    <a href="{{ route('segon.plantilla') }}">Plantilla</a>
+                    <a href="{{ route('segon.calendari') }}">Calendari</a>
+                    <a href="{{ route('segon.jornada') }}">Jornada</a>
+                    <a href="{{ route('segon.resultat') }}">Resultats</a>
+                    <a href="{{ route('segon.classificacio') }}">Classificació</a>
+                </div>
+            </li>
+            <li class="desplegable"><a href="{{ route('porters.index') }}">ESCOLA DE PORTERS</a>
+                <div class="desplegableContingut">
+                    <a href="{{ route('porters.formacio') }}">Formació i metodologia</a>
+                    <a href="{{ route('porters.horari') }}">Horari, calendari i esdeveniments</a>
+                    <a href="{{ route('porters.entrenadors') }}">Entrenadors</a>
+                    <a href="{{ route('porters.plans') }}">Plans i tarifes</a>
+                    <a href="{{ route('porters.contacte') }}">Contacte</a>
+                </div>
+            </li>
+            <li class="desplegable"><a href="{{ route('secretaria.index') }}">SECRETARIA</a>
+                <div class="desplegableContingut">
+                    <a href="{{ route('secretaria.oficina') }}">Oficina</a>
+                    <a href="{{ route('secretaria.inscripcions') }}">Inscripcions</a>
+                    <a href="{{ route('secretaria.merchandasing') }}">Merchandasing</a>
+                    <a href="{{ route('secretaria.normativa') }}">Normativa</a>
+                    <a href="{{ route('secretaria.contacte') }}">Contacte</a>
+                    <a href="{{ route('secretaria.acces') }}">Accés Secretaria</a>
+                </div>
+            </li>
+            <li class="desplegable"><a href="{{ route('historia.index') }}">HISTÒRIA</a>
+                <div class="desplegableContingut">
+                    <a href="{{ route('historia.ressenya') }}">Ressenya històrica</a>
+                    <a href="{{ route('historia.cronologia') }}">Cronologia d'èxits</a>
+                    <a href="{{ route('historia.fotografies') }}">Fotografies històriques</a>
+                    <a href="{{ route('historia.envians') }}">Envia'ns la teva foto</a>
+                </div>
+            </li>
+            <li><a href="{{ route('patrocinadors.index') }}">PATROCINADORS</a></li>
         </ul>
         <hr class="linea">
     </nav>
+</div>
+
 
     <div class="Introduccio1">
         <div class="Apartat">
@@ -153,5 +159,13 @@
         </div>
     </div>
 </div>
+<footer class="footer-public">
+    <p>📞 606 27 31 73 | ✉️ administracio@cfvila-seca.com</p>
+    <div class="social-icons">
+        <a href="https://facebook.com" target="_blank">Facebook</a> |
+        <a href="https://instagram.com" target="_blank">Instagram</a>
+    </div>
+</footer>
+
 
 @endsection
